609aeb41c406d9576c1aed3086cf64f63de15f7d
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-07-16 18:00:53 -0700

    Implement users table migration

--- express-back-end/db/migrations/20190712181741_create_users_table.js
+++ express-back-end/db/migrations/20190712181741_create_users_table.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex) {
-  
+  return Promise.all([
+    knex.schema.createTable('users', table => {
+      table.increments('id');
+      table.string('first_name');
+      table.string('last_name')
+      table.string('email');
+      table.string('password');
+    })
+  ])
 };
 
 exports.down = function(knex) {
-  
+  return Promise.all([
+    knex.schema.dropTable('users')
+  ])
 };
