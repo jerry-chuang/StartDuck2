4073a9cf7858842614f637e7e788574bf242d640
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-07-26 18:43:01 -0700

    Fix calendar link bug

--- react-front-end/src/TodayActivityCalendar.jsx
+++ react-front-end/src/TodayActivityCalendar.jsx
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import TodaySchedule from './TodaySchedule.jsx';
 import {Link} from "react-router-dom";
+import * as moment from 'moment';
 
 class TodayActivityCalendar extends Component {
   render() {
     const activities = this.props.activities.map(activity => {
         return (
-          <Link to={`/${activity.date}/activities/${activity.id}`} >
+          <Link to={`/${moment(activity.date).format('YYYY-MM-DD')}/activities/${activity.activity_id}`} >
             <TodaySchedule key = {activity.id} {...activity} /> 
           </Link>
         )
