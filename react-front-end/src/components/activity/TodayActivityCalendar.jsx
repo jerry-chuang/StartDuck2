2da557f33abd868cdc29e2055dd9f2ffed7abe75
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-08-09 11:26:24 -0700

    Convert to functional component

--- react-front-end/src/components/activity/TodayActivityCalendar.jsx
+++ react-front-end/src/components/activity/TodayActivityCalendar.jsx
@@ -1,24 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
 import TodaySchedule from './TodaySchedule.jsx';
 import {Link} from "react-router-dom";
 import * as moment from 'moment';
 
-class TodayActivityCalendar extends Component {
-  render() {
-    const activities = this.props.activities.map(activity => {
-        return (
-          <Link to={`/${moment(activity.date).format('YYYY-MM-DD')}/activities/${activity.user_activitiy_id}`} >
-            <TodaySchedule key = {activity.user_activitiy_id} {...activity} /> 
-          </Link>
-        )
-      })
+function TodayActivityCalendar() {
+  const activities = this.props.activities.map(activity => {
+    return (
+      <Link key={activity.activity_id} to={`/${moment(activity.date).format('YYYY-MM-DD')}/activities/${activity.user_activitiy_id}`} >
+        <TodaySchedule key = {activity.user_activitiy_id} {...activity} /> 
+      </Link>
+    )
+  })
 
-      return (
-        <main className="todayActivityList">
-          {activities}
-        </main>
-      );
-    }
-  }
+  return (
+    <main className="todayActivityList">
+      {activities}
+    </main>
+  );
+}
 
 export default TodayActivityCalendar;
