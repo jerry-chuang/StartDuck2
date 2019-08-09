72d30d9978c03903fc4558191627013497bd1ff0
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-08-09 11:33:39 -0700

    Remove unused css class

--- react-front-end/src/components/activity/TodaySchedule.jsx
+++ react-front-end/src/components/activity/TodaySchedule.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const TodaySchedule = (props) => {
-    return (
-        <div className="todayActivityItem">
-          <span className="dayActivities_itemName">{props.name}</span>
-          <input type="checkbox" disabled={true} checked={props.is_complete} />
-        </div>
-    )
+  return (
+    <div className="todayActivityItem">
+      <span>{props.name}</span>
+      <input type="checkbox" disabled={true} checked={props.is_complete} />
+    </div>
+  )
 }
 
 export default TodaySchedule
