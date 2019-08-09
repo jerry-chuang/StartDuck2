12ab4e77d71cf4d3bfbb4a1b1a11900d4385485b
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-08-08 18:44:54 -0700

    Convert Login.jsx into functional component

--- react-front-end/src/components/Login.jsx
+++ react-front-end/src/components/Login.jsx
@@ -6,68 +6,69 @@ import {Redirect} from "react-router-dom";
 import * as moment from 'moment';
 import axios from 'axios';
 
-class Login extends Component {
+function Login (props) {
+  const {cookies, form, setUser} = props;
 
-  handleSubmit = e => {
+  function handleSubmit (e) {
     e.preventDefault();
-    this.props.form.validateFields((err, values) => {
+    form.validateFields((err, values) => {
       if (!err) {
         axios
         .post('/api/users', {email: values.email})
-        .then(results => {
-          this.props.cookies.set("email", values.email, { path: '/' })
-          this.props.setUser(this.props.cookies.get('email'))  
+        .then(() => {
+          cookies.set("email", values.email, { path: '/' })
+          setUser(cookies.get('email'))  
         })  
       }
     });
   };
 
-  render() {
-    if(this.props.cookies.get('email')){
-      return (
-          <Redirect to={`/${moment().format('YYYY-MM-DD')}/activities`}/>
-      )
-    } 
-    const { getFieldDecorator } = this.props.form;
+  if(cookies.get('email')){
     return (
-      <section className="login-page">
-        <div>
-          {/* <MainImage className="main_image" /> */}
-          <img src={main} alt ="Main-page"className="main_image" />
-        </div>
-        <Form onSubmit={this.handleSubmit} className="login-form">
-        <h1>Manage your time with us!</h1>
-          <Form.Item>
-            {getFieldDecorator('email', {
-              rules: [{ required: true, message: 'Please input your email!' }],
-            })(
-              <Input
-                prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                placeholder="E-mail address"
-              />,
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Please input your Password!' }],
-            })(
-              <Input
-                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="password"
-                placeholder="Password"
-              />,
-            )}
-          </Form.Item>
-          <Form.Item>
-            <Button htmlType="submit" className="login-page_login-button">
-              Log in
-            </Button>     
-          </Form.Item>
-        </Form>
-      </section>
-    );
-  }
+        <Redirect to={`/${moment().format('YYYY-MM-DD')}/activities`}/>
+    )
+  } 
+  const { getFieldDecorator } = form;
+  return (
+    <section className="login-page">
+      <div>
+        {/* <MainImage className="main_image" /> */}
+        <img src={main} alt ="Main-page"className="main_image" />
+      </div>
+      <Form onSubmit={handleSubmit} className="login-form">
+      <h1>Manage your time with us!</h1>
+        <Form.Item>
+          {getFieldDecorator('email', {
+            rules: [{ required: true, message: 'Please input your email!' }],
+          })(
+            <Input
+              prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="E-mail address"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          <Button htmlType="submit" className="login-page_login-button">
+            Log in
+          </Button>     
+        </Form.Item>
+      </Form>
+    </section>
+  );
+  
 }
+
 const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(Login);
 
 export default WrappedNormalLoginForm;
