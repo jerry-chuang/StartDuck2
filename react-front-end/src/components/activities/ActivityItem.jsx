7e6bb0c3ee9f6b9f46fdd47cd7221be0d5b27332
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-08-08 19:37:29 -0700

    Destructure variables from props

--- react-front-end/src/components/activities/ActivityItem.jsx
+++ react-front-end/src/components/activities/ActivityItem.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-
 const ActivityItem = (props) => {
-    return (
-        <div className="activities_activityItem">
-          <span>{props.duration} m</span>
-          <span>{props.name}</span>
-          <input type="checkbox" disabled={true} checked={props.is_complete} />
-        </div>
-    );
+  const {duration, name, is_complete} = props;
+  return (
+    <div className="activities_activityItem">
+      <span>{duration} m</span>
+      <span>{name}</span>
+      <input type="checkbox" disabled={true} checked={is_complete} />
+    </div>
+  );
 }
 
 export default ActivityItem;
