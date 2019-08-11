ad46de65f6619ae6cb118e7289127488fb96d138
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-08-11 12:45:07 -0700

    Linting

--- react-front-end/src/components/completeActivities/CompletedActivitiesItem.jsx
+++ react-front-end/src/components/completeActivities/CompletedActivitiesItem.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const CompletedActivitiesItem = (props) => {
-    return (
-        <div className="activities_activityItem">
-          <span>{props.duration} m</span>
-          <span >{props.name}</span>
-          <input type="checkbox" disabled={true} checked={props.completeness} />
-        </div>
-    );
+  return (
+    <div className="activities_activityItem">
+      <span>{props.duration} m</span>
+      <span >{props.name}</span>
+      <input type="checkbox" disabled={true} checked={props.completeness} />
+    </div>
+  )
 }
 
 export default CompletedActivitiesItem;
