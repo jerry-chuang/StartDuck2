717022adb74ea9d9d88f32a34e9a208adb35d403
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-08-11 20:57:02 -0700

    Convert to functional component

--- react-front-end/src/components/completeActivities/CompletedActivitiesList.jsx
+++ react-front-end/src/components/completeActivities/CompletedActivitiesList.jsx
@@ -1,22 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
 import CompletedActivitiesItem from './CompletedActivitiesItem.jsx'
 import {Link} from "react-router-dom";
 
-
-class CompletedActivitiesList extends Component {
-  render() {
-    const activities = this.props.activities.map(activity => {
-      return <Link to={`/completed_activities/${activity.id}`} >
-                <CompletedActivitiesItem key = {activity.id} {...activity } />
-             </Link>
-    })
-
+function CompletedActivitiesList(props) {
+  console.log(props)
+  const activities = props.activities.map(activity => {
     return (
-      <main className="dayActivities_activitiesList">
-        {activities}
-      </main>
-    );
-  }
+      <Link to={`/completed_activities/${activity.id}`} >
+        <CompletedActivitiesItem key = {activity.id} {...activity } />
+      </Link>
+    )
+  })
+
+  return (
+    <main className="dayActivities_activitiesList">
+      {activities}
+    </main>
+  )
 }
 
 export default CompletedActivitiesList;
