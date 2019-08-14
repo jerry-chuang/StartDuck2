2915968f14143057f7eabaa818feb5d6b32dfc8d
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-08-13 18:16:28 -0700

    Convert to react hooks

--- react-front-end/src/components/schedule/CategoryPicker.jsx
+++ react-front-end/src/components/schedule/CategoryPicker.jsx
@@ -1,66 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Checkbox } from 'antd';
 import SlideToggle from "react-slide-toggle";
 import axios from 'axios';
 
+function CategoryPicker(props) {
+  const [categories, setCategories] = useState([]);
+  const {pickCategories} = props;
 
-class CategoryPicker extends React.Component{
-
-    constructor(props) {
-      super(props)
-      this.state = {
-        categories: []
-    }
-  }
-
-    componentDidMount() {
-        axios.get('/api/categories')
-        .then((response) => {
-          console.log('response from api/categoreis', response)
-          this.setState({
-            categories: response.data.categories
-          });
-        })
-      }
-
-    sendSelectedCategories = e => {
-      this.props.onSelectedCategories(e.target.value)
-    }
-
-    render() {
-      const categories = this.state.categories.map(category =>{
-        return( <Checkbox
-                 key={ category.id }
-                 value={ category.id }
-                 onChange={ this.sendSelectedCategories }
-                 >
-                 { category.name }
-                 </Checkbox> )
+  useEffect(()=>{
+    axios.get('/api/categories')
+    .then((response) => {
+      setCategories(response.data.categories);
     })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])// run only once on mount
 
-      return(
-        <SlideToggle
-          duration={800}
-          collapsed
-          render={({ onToggle, setCollapsibleElement}) => (
-            <div className="my-collapsible">
-              <button className="my-collapsible__toggle" onClick={onToggle}>
-                Select Categories
-              </button>
-              <div className="my-collapsible__content" ref={setCollapsibleElement}>
-                <div className="my-collapsible__content-inner">
-                  <div className="radio_style">
-                    { categories }
-                  </div>
-                </div>
+  const categoryOptions = categories.map(category =>{
+    return(
+      <Checkbox
+      key={ category.id }
+      value={ category.id }
+      onChange={ pickCategories }
+      >
+        { category.name }
+      </Checkbox> )
+  })
+
+  return(
+    <SlideToggle
+      duration={800}
+      collapsed
+      render={({ onToggle, setCollapsibleElement}) => (
+        <div className="my-collapsible">
+          <button className="my-collapsible__toggle" onClick={onToggle}>
+            Select Categories
+          </button>
+          <div className="my-collapsible__content" ref={setCollapsibleElement}>
+            <div className="my-collapsible__content-inner">
+              <div className="radio_style">
+                { categoryOptions }
               </div>
             </div>
-          )}
-        />
-      )
-   }
-  };
+          </div>
+        </div>
+      )}
+    />
+  )  
+};
 
 export default CategoryPicker;
-
-
